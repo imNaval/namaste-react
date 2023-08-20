375ab02f52074ba2f6f6be3428f7b7c0fd75629d
imNaval <nehranvl2017@gmail.com>
2023-08-20 19:32:30 +0530

    episode-01 Inception

---
+++ App.js
@@ -0,0 +1,21 @@
+const heading = React.createElement("h1", {id:"heading"}, "Hello World, From React");
+
+//create nested child and sibling
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [
+            React.createElement("h1", {key:"h1"}, "I'm h1 tag"),
+            React.createElement("h2", {key:"h2"}, "I'm h2 tag")
+        ]
+    )
+)
+
+console.log(heading);
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
