fe485a4911b59c32b2ab71fa819b85cd15eb837f
imNaval <nehranvl2017@gmail.com>
2023-08-21 23:10:27 +0530

    episode-02 Igniting our App

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {id:"heading"}, "Hello World, From React");
 
 //create nested child and sibling
