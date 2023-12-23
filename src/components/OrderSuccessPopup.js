8a03ef0612e0887d8b634f54b8f178f043185cc3
imNaval <nehranvl2017@gmail.com>
2023-12-23 23:05:09 +0530

    &#8377;

--- src/components/OrderSuccessPopup.js
+++ src/components/OrderSuccessPopup.js
@@ -16,7 +16,7 @@ const OrderSuccessPopup = ({ totalPrice, onClose }) => {
     <div className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${isVisible ? 'visible' : 'invisible'}`}>
       <div className="bg-white p-16 rounded-md shadow-md">
         <h2 className="text-2xl font-bold mb-4">Order Placed Successfully!</h2>
-        <p>Your order total: ${totalPrice}</p>
+        <p>Your order total: &#8377;{totalPrice}</p>
         <p>Thank you for your order!</p>
       </div>
     </div>
